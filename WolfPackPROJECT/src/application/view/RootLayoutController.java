0e09b817a442df7d6e80a4d2394ec79a2909a8cd
CruzCarlos <57798802+CruzCarlos@users.noreply.github.com>
2019-12-08 18:49:01 +0100

    Add files via upload

---
+++ WolfPackPROJECT/src/application/view/RootLayoutController.java
@@ -0,0 +1,14 @@
+package application.view;
+import application.Main;
+
+
+public class RootLayoutController {
+	
+    private Main mainApp;
+    
+    
+    public void setMainApp(Main mainApp) {
+        this.mainApp = mainApp;
+    }
+
+}
