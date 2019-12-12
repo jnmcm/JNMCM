6ae32da8b8d7e5a14d3bdb7993bb4186cf834b3f
nbelhidane <n.belhidane@gmail.com>
2019-12-12 21:33:07 +0100

    Sliders change the graph

--- WOLFPACK/src/application/view/LineGraphController.java
+++ WOLFPACK/src/application/view/LineGraphController.java
@@ -6,11 +6,15 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+
 
 public class LineGraphController implements Initializable{
 	
@@ -29,13 +33,27 @@ public class LineGraphController implements Initializable{
 	@FXML
     private LineChart<String, Number> lineChart;
 	
+	@FXML
+	private Label nbWolves;
+	
+	//add other labels later 
 	
 	private Main mainApp;
 	
 	
 //	Use this method to fill the line graph
-	public void setGraphData() {
+	public XYChart.Series<String, Number> wolfData() {
+		XYChart.Series<String, Number> wolfData = new XYChart.Series<String, Number>();
+		wolfData.setName("Gray Wolves");
+		wolfData.getData().add(new XYChart.Data<String, Number>("2013", Integer.valueOf(nbWolves.getText())));
+		wolfData.getData().add(new XYChart.Data<String, Number>("2014", Integer.valueOf(nbWolves.getText())));
+		wolfData.getData().add(new XYChart.Data<String, Number>("2015", Integer.valueOf(nbWolves.getText())));
+		wolfData.getData().add(new XYChart.Data<String, Number>("2016", Integer.valueOf(nbWolves.getText())));
+		wolfData.getData().add(new XYChart.Data<String, Number>("2017", Integer.valueOf(nbWolves.getText())));
+		wolfData.getData().add(new XYChart.Data<String, Number>("2018", Integer.valueOf(nbWolves.getText())));
+		wolfData.getData().add(new XYChart.Data<String, Number>("2019", Integer.valueOf(nbWolves.getText())));
 		
+		return wolfData;
 	}
 	
 //	Method not necessary at the moment
@@ -47,8 +65,9 @@ public class LineGraphController implements Initializable{
 	//no idea how you guys would do it with the other method. this works for now.
 	public void initialize(URL url, ResourceBundle rb) {
 		lineChart.getData().clear();
+		nbWolves.setText("0");
 		XYChart.Series<String, Number> deerData = new XYChart.Series<String, Number>();
-		deerData.setName("Deer");
+		deerData.setName("Red Deer");
 		deerData.getData().add(new XYChart.Data<String, Number>("2013", 2125));
 		deerData.getData().add(new XYChart.Data<String, Number>("2014", 3150));
 		deerData.getData().add(new XYChart.Data<String, Number>("2015", 3050));
@@ -59,7 +78,7 @@ public class LineGraphController implements Initializable{
 		lineChart.getData().add(deerData);
 		
 		XYChart.Series<String, Number> horseData = new XYChart.Series<String, Number>();
-		horseData.setName("Horses");
+		horseData.setName("Konik Horses");
 		horseData.getData().add(new XYChart.Data<String, Number>("2013", 900));
 		horseData.getData().add(new XYChart.Data<String, Number>("2014", 1350));
 		horseData.getData().add(new XYChart.Data<String, Number>("2015", 1075));
@@ -80,20 +99,22 @@ public class LineGraphController implements Initializable{
 		cattleData.getData().add(new XYChart.Data<String, Number>("2019", 280));
 		lineChart.getData().add(cattleData);
 		
-		XYChart.Series<String, Number> wolfData = new XYChart.Series<String, Number>();
-		wolfData.setName("Wolves");
-		wolfData.getData().add(new XYChart.Data<String, Number>("2013", 500));
-		wolfData.getData().add(new XYChart.Data<String, Number>("2014", 500));
-		wolfData.getData().add(new XYChart.Data<String, Number>("2015", 500));
-		wolfData.getData().add(new XYChart.Data<String, Number>("2016", 500));
-		wolfData.getData().add(new XYChart.Data<String, Number>("2017", 500));
-		wolfData.getData().add(new XYChart.Data<String, Number>("2018", 500));
-		wolfData.getData().add(new XYChart.Data<String, Number>("2019", 500));
-		lineChart.getData().add(wolfData);
+
+		lineChart.getData().add(wolfData());
+		
+		 firstSlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+             @Override
+             public void changed(
+                ObservableValue<? extends Number> observableValue, 
+                Number oldValue, 
+                Number newValue) { 
+                   nbWolves.textProperty().setValue(
+                        String.valueOf(newValue.intValue()));
+                   lineChart.getData().set(3, wolfData());
+               }
+     });
 	}
-	
-	
-	
 	
 	
 }
