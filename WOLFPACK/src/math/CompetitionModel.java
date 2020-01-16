bfd37ac94a3a4e2150601efa7c48ea6edb3039bb
nbelhidane <n.belhidane@gmail.com>
2020-01-16 22:57:41 +0100

    added LV model

---
+++ WOLFPACK/src/math/CompetitionModel.java
@@ -0,0 +1,15 @@
+package math;
+
+import domain.Animal;
+
+public class CompetitionModel extends LotkaVolterraModel implements IModels {
+	//Use the formula sheet I sent you @Carlos
+	//Check LotkaVolterraModel class for example
+	//we need capture efficiency, for now, you can use random data I guess
+	
+	@Override
+	public int getCalculatedPopulation(Animal animal) {
+		return 0;
+	}
+
+}
